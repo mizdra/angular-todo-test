39b9920ef090e15daee8e6b1b2de45df29a39c66
mizdra <pp.mizdra@gmail.com>
2016-03-27 23:07:38 +0900

    second commit

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -3,4 +3,37 @@
 
 import angular = require('angular');
 
-angular.module('App', []);
+angular.module('App', [])
+.controller('MainController', ['$scope', '$filter', function ($scope, $filter) {
+  $scope.todos = [];
+
+  $scope.newTitle = '';
+
+  $scope.addTodo = function () {
+    $scope.todos.push({
+      title: $scope.newTitle,
+      done: false
+    });
+
+    $scope.newTitle = '';
+  };
+
+  $scope.filter = {
+    done: { done: true },
+    remaining: { done: false }
+  };
+  $scope.currentFilter = undefined;
+
+  $scope.changeFilter = function (filter) {
+    $scope.currentFilter = filter;
+  };
+
+  var where = $filter('filter');
+  $scope.$watch('todos', function (todos) {
+    var length = todos.length;
+
+    $scope.allCount = length;
+    $scope.doneCount = where(todos, $scope.filter.done).length;
+    $scope.remainingCount = length - $scope.doneCount;
+  }, true);
+}]);
